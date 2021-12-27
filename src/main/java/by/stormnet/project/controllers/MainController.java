a51831c05bed8f1089240286324c2e4484ca2790
Stormnet-Student <sergeinait@gmail.com>
2021-12-27 20:31:32 +0300

    change text

--- src/main/java/by/stormnet/project/controllers/MainController.java
+++ src/main/java/by/stormnet/project/controllers/MainController.java
@@ -73,12 +73,12 @@ public class MainController {
 
     @FXML
     protected void onReadTextButtonClick() {
-        textField.setText("Hello word!");
+        textField.setText("Hello java!");
     }
 
     @FXML
     protected void onWriteTextButtonClick() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Info");
         alert.setHeaderText(textField.getText());
         Optional<ButtonType> buttonType = alert.showAndWait();
